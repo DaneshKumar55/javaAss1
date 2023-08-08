6c8abf011492fc62472e7f245bf83d91fc869a64
Danesh Kumar <kumardanesh012@gmail.com>
2023-08-08 21:51:37 +0500

    First

---
+++ java.js
@@ -0,0 +1,14 @@
+var cities = [];
+console.log(cities);
+
+var cities = [,,,,,"info@gmail.com"];
+console.log(cities);
+
+var cities = [,"iHunar",,,,"info@gmail.com"];
+console.log(cities);
+
+cities[55]="Skillers";
+console.log(cities);
+
+var cities = prompt("Type your city name");
+console.log("Your city name is: " + cities);
